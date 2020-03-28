5bd2503cbdb4b7b10493a513fcbdf56669f50d46
codewithgurpreet <codewithgurpreet@gmail.com>
2020-03-28 16:15:54 +0000

    Initial commit for Number Methods.py

---
+++ Primitive Types/Number Methods.py
@@ -0,0 +1,18 @@
+# round() Function - rounding a number
+print(round(2.9))
+
+# abs() Function - absolute(postive) value of a number
+print(abs(-2.9))
+
+# Only a handful of useful functions to work with numbers
+# For more complex operations use 'math' module
+# 'math' module contains lot of mathematical functions
+# In order to use it, you will need to import it first into your program
+
+import math
+
+# Then use the dot notation ot access the various maths functions
+print(math.log(3, 100))   # log(x, base)
+print(math.factorial(10)) # return factorial of a non-negative whole number
+
+# To learn more google 'Python 3 math module'
