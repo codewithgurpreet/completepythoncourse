9adcc8e42f7eaaa8934abe7189bb578092f40dc0
codewithgurpreet <codewithgurpreet@gmail.com>
2020-03-28 15:23:48 +0000

    Initial commit for Escape Sequences.py

---
+++ Primitive Types/Escape Sequences.py
@@ -0,0 +1,24 @@
+course = "Python Programming"
+
+# What if you want to insert a double quote in your string?
+# There are two ways:
+
+# First one - use single quotes for the string
+course = 'Python "Programming'
+# Second one - use \ escape character
+course = "Python \"Programming"
+print(course)
+
+# Other common escape sequences
+# \"
+course = "Python \"Programming"
+print(course)
+# \'
+course = "Python \'Programming"
+print(course)
+# \\
+course = "Python \\Programming"
+print(course)
+# \n
+course = "Python \nProgramming"
+print(course)
