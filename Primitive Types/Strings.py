b15854dd77b49a1231c0a4bdec2f408967493f8e
codewithgurpreet <codewithgurpreet@gmail.com>
2020-03-28 15:22:45 +0000

    Initial commit for Strings.py

---
+++ Primitive Types/Strings.py
@@ -0,0 +1,27 @@
+# Standard string - single or doubvle quotes
+
+course = "Python Programming"
+
+# Multiline String - triple quotes
+message = """
+Hi John
+
+This is Gurpreet from code with Gurpreet.
+blah blah blah....
+"""
+
+# String Functions
+# think of functions on your remote control - build in functions - used to perform various tasks
+
+print(len(course))
+
+# Strings are 0 indexed
+print(course[0])  # first character
+print(course[-1])  # first character from the end of the string
+
+# Slicing
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+print(course[:])
+
