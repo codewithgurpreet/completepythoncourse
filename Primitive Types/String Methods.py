a839615311b3708921b597672dd8fb58bdd63192
codewithgurpreet <codewithgurpreet@gmail.com>
2020-03-28 15:09:10 +0000

    Initial commit for Primitive Types folder from PyCharm

---
+++ Primitive Types/String Methods.py
@@ -0,0 +1,7 @@
+# Common functions available to work with strings
+
+# len() function - length of the string
+# len() function is general purpose and is not limited to strings
+# later in the course we will learn about othere uses
+course = "Python Programming"
+print(len(course))
