2b20a116e3c09f83e320c007fe58b47810289162
codewithgurpreet <codewithgurpreet@gmail.com>
2020-03-28 16:06:28 +0000

    Initial commit for Numbers.py

---
+++ Primitive Types/Numbers.py
@@ -0,0 +1,27 @@
+# In Python we have three types of numbers:
+
+x = 1      # Integer
+print(x)
+
+y = 1.1    # Float
+print(y)
+
+z = 2 + 3j # Complex numbers - represented in the form a + bj
+print(z)
+
+# Standard Arithmetic Operations
+
+print(10 + 3)    # Addition
+print(10 - 3)    # Substraction
+print(10 * 3)    # Multiplication
+print(10 / 3)    # Division
+print(10 % 3)    # Division with only remainder returned
+print(10 // 3)   # Integer division
+print(10 ** 3)   # Exponent
+
+# Augmented assignment operator - valid for all the above operations
+x = 10
+x = x + 3
+x += 3         # This is the augmented assignment operator -> shortcut for x = x + 3
+
+
