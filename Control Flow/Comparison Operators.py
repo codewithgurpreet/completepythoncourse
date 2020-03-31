4b7bafbc32b884d9ec498b1c14928ab0246ffb32
Code With Gurpreet <codewithgurpreet@gmail.com>
2020-03-31 14:08:25 +0100

    Initial commit for Comparison Operators.py

---
+++ Control Flow/Comparison Operators.py
@@ -0,0 +1,30 @@
+# Comparison Operators are used to compare values
+# Common comparison operators
+# >
+# <
+# <=
+# >=
+# ==
+# !=
+
+# Comparison operators result in Boolean Expressions - Result is True or False
+# Examples
+10 > 3 <- True
+10 >= 3 <- True
+10 < 20 <- True
+10 <= 20 <- True
+10 == 10 <- True
+10 == "10" <- False
+10 != "10" <- True
+
+# We can also use these comparison operators with strings
+"bag" > "apple" <- True
+"bag" == "BAG"  <- False
+# The reason is that b and B are different
+# check using the ord() function
+
+ord("b") <- 98
+ord("B") <- 66
+
+
+
